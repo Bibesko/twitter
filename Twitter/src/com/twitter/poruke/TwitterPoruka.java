1484e7f00b9629cfce27f7e1f856bda8b115eb55
Bibesko <bibesko94@gmail.com>
2015-03-28 18:18:05 +0100

    Uradjena dokumentacija za klasu TwitterPoruke i za sve atribute i metode u okviru nje.

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,24 +1,58 @@
 package com.twitter.poruke;
 
+/**
+ * Predstavlja poruke sa Twittera.
+ * 
+ * @author bibesko
+ * @version v1.0
+ *
+ */
 public class TwitterPoruka {
 
+	/**
+	 * Nickname korisnika.
+	 */
 	private String korisnik;
+	
+	/**
+	 * Tekst poruke.
+	 */
 	private String poruka;
 
+	/**
+	 * Metoda vraca nickname korisnika tj. vrednost atributa korisnik.
+	 * @return nickname korisnika kao string.
+	 */
 	public String getKorisnik() {
 		return korisnik;
 	}
 
+	/**
+	 * Metoda postavlja nickname korisnika na zadatu vrednost.
+	 * @param novi nickname korisnika.
+	 * @throws java.lang.RuntimeException ako je unet 
+	 * prazan string ili null vrednost.
+	 */
 	public void setKorisnik(String korisnik) {
 		if (korisnik == null || korisnik.equals(""))
 			throw new RuntimeException("Ime korisnika mora biti uneto");
 		this.korisnik = korisnik;
 	}
 
+	/**
+	 * Metoda vraca proruku to jest vrednost atributa poruka.
+	 * @return poruka kao string.
+	 */
 	public String getPoruka() {
 		return poruka;
 	}
 
+	/**
+	 * Metoda postavlja vrednost poruke na zadatu vrednost iz parametra.
+	 * @param poruka nova poruka.
+	 * @throws java.lang.RuntimeException ako je unet prazan string 
+	 * ili null vrednost.
+	 */
 	public void setPoruka(String poruka) {
 		if (poruka == null || poruka.length() > 140)
 			throw new RuntimeException(
@@ -26,6 +60,10 @@ public class TwitterPoruka {
 		this.poruka = poruka;
 	}
 
+	/**
+	 * Metoda vraca vrednosti za korisnika vreme i poruku i
+	 * odgovarajuci tekst uz njih.
+	 */
 	public String toString() {
 		return "KORISNIK:" + korisnik + " VREME:" + vreme.getTime()
 				+ " PORUKA:" + poruka;
